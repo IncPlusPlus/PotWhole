375578c8a6e1413426dc3de52ad70d8df2d0dfb8
serpawatwit <serpaw@wit.edu>
2022-07-31 21:16:53 -0400

    updated firebase functions
    added firebase functions for getting all reports, updating user info, getting user info
    Added pages and buttons corresponding to the functions

--- app/src/main/java/io/github/incplusplus/potwhole/FirebaseFunctionExample.java
+++ app/src/main/java/io/github/incplusplus/potwhole/FirebaseFunctionExample.java
@@ -1,6 +1,10 @@
 package io.github.incplusplus.potwhole;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.Button;
 import androidx.annotation.Nullable;
@@ -8,7 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +26,17 @@ public class FirebaseFunctionExample extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseFunctions mFunctions;
 
+    // Uri indicates, where the image will be picked from
+    private Uri filePath;
+
+    // bitmap for image grabbed from gallery
+    private Bitmap bitmap;
+    private Bitmap downloadImage;
+
+    // request code
+    private final int PICK_IMAGE_REQUEST = 22;
+
+    private String downloadURL;
     private String reportId = "";
 
     private double INSERT_REPORT_LATITUDE, INSERT_REPORT_LONGITUDE;
@@ -26,9 +46,22 @@ public class FirebaseFunctionExample extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase_function_example);
 
+        Button uploadImage = findViewById(R.id.uploadImage);
+
         Button createPost = findViewById(R.id.createPost);
 
         Button getPost = findViewById(R.id.getPost);
+
+        Button getPosts = findViewById(R.id.getPosts);
+
+        uploadImage.setOnClickListener(
+                v -> {
+                    try {
+                        sendImage();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
 
         createPost.setOnClickListener(
                 v -> {
@@ -39,7 +72,87 @@ public class FirebaseFunctionExample extends AppCompatActivity {
                 v -> {
                     getReport();
                 });
+
+        getPosts.setOnClickListener(
+                v -> {
+                    getAllReports();
+                });
     }
+
+    private void sendImage() throws IOException {
+
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(
+                Intent.createChooser(intent, "Select Image from here..."), PICK_IMAGE_REQUEST);
+    }
+
+    // Override onActivityResult method
+    // Method from GeeksOfGeeks for testing upload.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // checking request code and result code
+        // if request code is PICK_IMAGE_REQUEST and
+        // resultCode is RESULT_OK
+        // then set image in the image view
+        if (requestCode == PICK_IMAGE_REQUEST
+                && resultCode == RESULT_OK
+                && data != null
+                && data.getData() != null) {
+
+            // Get the Uri of data
+            filePath = data.getData();
+            try {
+
+                // Setting image on image view using Bitmap
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                uploadImage();
+            } catch (IOException e) {
+                // Log the exception
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+
+        // create a reference to complete image path
+        StorageReference imagesUploadRef = storageRef.child("reports/reportImages/test.jpg");
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+
+        UploadTask uploadTask = imagesUploadRef.putBytes(data);
+        uploadTask
+                .addOnFailureListener(
+                        exception -> {
+                            // Handle unsuccessful uploads
+                        })
+                .addOnSuccessListener(
+                        taskSnapshot -> {
+                            // taskSnapshot.getMetadata() contains file metadata such as size,
+                            // content-type, etc.
+                            // ...
+                        });
+
+        imagesUploadRef
+                .getDownloadUrl()
+                .addOnSuccessListener(
+                        uri -> {
+                            downloadURL = uri.toString();
+                            Log.v("MY_URL", downloadURL);
+                            // sendToBitmap();
+                        });
+    }
+
+    private void sendToBitmap() {}
 
     private void createReport() {
 
@@ -57,6 +170,7 @@ public class FirebaseFunctionExample extends AppCompatActivity {
 
         Map<String, Object> data = new HashMap<>();
         data.put("title", "INSERT_REPORT_TITLE");
+        data.put("image", downloadURL);
         data.put("description", "INSERT_REPORT_DESCRIPTION");
         data.put("location", locationDevice);
 
@@ -104,6 +218,7 @@ public class FirebaseFunctionExample extends AppCompatActivity {
 
         Map<String, Object> data = new HashMap<>();
         data.put("rid", reportId);
+        Log.v("RID", reportId);
 
         Gson gson = new Gson();
         final String jsonData = gson.toJson(data);
@@ -125,6 +240,34 @@ public class FirebaseFunctionExample extends AppCompatActivity {
                                     "REPORT_GET",
                                     "Return From Database - "
                                             + httpsCallableResult.getData().toString());
+
+                            Map<String, Object> dataFromDatabase = new HashMap<>();
+                            dataFromDatabase.putAll(
+                                    (Map<? extends String, ?>) httpsCallableResult.getData());
+                        });
+    }
+
+    private void getAllReports() {
+
+        mAuth = FirebaseAuth.getInstance();
+        mFunctions = FirebaseFunctions.getInstance();
+
+        Log.v("REPORT_GET", "Getting all reports from database...");
+
+        mFunctions
+                .getHttpsCallable("getAllReportDocuments")
+                .call()
+                .addOnFailureListener(
+                        e -> {
+                            Log.v("REPORT_GET", "Getting Report Document Failed");
+                            Log.v("REPORT_GET", "Exception - " + e);
+                        })
+                .addOnSuccessListener(
+                        httpsCallableResult -> {
+                            Log.v("REPORT_GET", "Getting Report Document Successful");
+                            Log.v(
+                                    "REPORT_GET",
+                                    "Return From Database - " + httpsCallableResult.getData());
 
                             Map<String, Object> dataFromDatabase = new HashMap<>();
                             dataFromDatabase.putAll(
