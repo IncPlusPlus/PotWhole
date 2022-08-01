67726e77dca80ec8f6efbc31d38caff716c76420
Mark Ren <72276067+markren77@users.noreply.github.com>
2022-08-01 13:45:22 -0400

    Untested code due to gradle sync errors, Code in Camera Fragment should be moved into to the class where the button to start the camera is at.

--- app/src/main/java/io/github/incplusplus/potwhole/CameraActivity.java
+++ app/src/main/java/io/github/incplusplus/potwhole/CameraActivity.java
@@ -1,6 +1,12 @@
 package io.github.incplusplus.potwhole;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.util.Size;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,6 +14,8 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -17,18 +25,29 @@ import androidx.lifecycle.LifecycleOwner;
 
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.nio.ByteBuffer;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class CameraActivity extends AppCompatActivity{
+    //Bunch of classes that are needed to display a camera view
+    private Executor executor;
     private PreviewView previewView;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private TextView textView;
+
+    Button captureButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        executor = Executors.newSingleThreadExecutor();
+        captureButton = findViewById(R.id.captureButton);
         previewView = findViewById(R.id.previewView);
+
+        //Needed for cameraX
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(new Runnable() {
             @Override
@@ -45,6 +64,7 @@ public class CameraActivity extends AppCompatActivity{
 
 
     private void bindImageAnalysis(@NonNull ProcessCameraProvider cameraProvider) {
+        //Resolution can be changed if wanted
         ImageAnalysis imageAnalysis =
                 new ImageAnalysis.Builder().setTargetResolution(new Size(1280, 720))
                         .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
@@ -54,11 +74,58 @@ public class CameraActivity extends AppCompatActivity{
                 image.close();
             }
         });
+        //Camera is displayed in Preview
         Preview preview = new Preview.Builder().build();
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+
+        //For the capturing Image
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+
+        //Picking which cam to use
         CameraSelector cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        //Need to set a rotation for whatever reason
+        final ImageCapture imageCapture = builder
+                .setTargetRotation(this.getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+
+        //Binding all the above to ListenableFuture
         cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector,
-                imageAnalysis, preview);
+                imageAnalysis, preview, imageCapture);
+
+
+        captureButton.setOnClickListener(v->{
+            imageCapture.takePicture(executor, new ImageCapture.OnImageCapturedCallback() {
+                @Override
+                public void onCaptureSuccess(@NonNull ImageProxy image) {
+                    super.onCaptureSuccess(image);
+                    //If this doesn't work it's something to do with needing to set rotation on the images taken
+                    Bitmap bitmap = toBitmap(image);
+                    Intent intent = new Intent(CameraActivity.this, ImageConfirmation.class);
+                    intent.putExtra("BitmapImage", bitmap);
+                    startActivity(intent);
+
+                }
+                @Override
+                public void onError(@NonNull ImageCaptureException error){
+                    error.printStackTrace();
+                }
+            });
+
+        });
+
+
+    }
+
+    //StackOverflow function for converting the taken Image to a bitmap
+    private Bitmap toBitmap(ImageProxy image) {
+        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+        buffer.rewind();
+        byte[] bytes = new byte[buffer.capacity()];
+        buffer.get(bytes);
+        byte[] clonedBytes = bytes.clone();
+        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
     }
 }
