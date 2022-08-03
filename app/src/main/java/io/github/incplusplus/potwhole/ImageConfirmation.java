2436fdfba91dca91f3176cb2f9a384017a4f4a73
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-08-03 11:27:50 -0400

    Finish camera implementation (see commit message)
    - We can now take a picture and bring it to the next activity
    - It runs like shit but that might just be because it's running inside the emulator

--- app/src/main/java/io/github/incplusplus/potwhole/ImageConfirmation.java
+++ app/src/main/java/io/github/incplusplus/potwhole/ImageConfirmation.java
@@ -2,10 +2,15 @@ package io.github.incplusplus.potwhole;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.IOException;
 
 public class ImageConfirmation extends AppCompatActivity {
 
@@ -16,14 +21,45 @@ public class ImageConfirmation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_image_preview);
 
+        Uri photoUri = getIntent().getParcelableExtra("photoUri");
+
         Intent intent = getIntent();
-        Bitmap bitmap = (Bitmap) intent.getParcelableExtra("BitmapImage");
+
+        ExifInterface ei = null;
+        try {
+            ei = new ExifInterface(photoUri.getPath());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int orientation =
+                ei.getAttributeInt(
+                        ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+
+        Bitmap bitmap = BitmapFactory.decodeFile(photoUri.getPath());
+        Bitmap rotatedBitmap = null;
+        switch (orientation) {
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = bitmap;
+        }
 
         Button yes = findViewById(R.id.useImage);
         Button no = findViewById(R.id.retakeImage);
 
         ImageView displayImage = findViewById(R.id.imagePreview);
-        displayImage.setImageBitmap(bitmap);
+        displayImage.setImageBitmap(rotatedBitmap);
 
         yes.setOnClickListener(
                 v -> {
@@ -36,5 +72,13 @@ public class ImageConfirmation extends AppCompatActivity {
                     Intent intent2 = new Intent(ImageConfirmation.this, CameraActivity.class);
                     startActivity(intent2);
                 });
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        // Could result in OOM error for large bitmaps as two will exist in memory at once
+        return Bitmap.createBitmap(
+                source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 }
