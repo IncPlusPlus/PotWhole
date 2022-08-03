9a853eb5c08b4176ca1d6c6711f6aca8dfe627b6
Ryan Cloherty <cloherty.ryan@gmail.com>
2022-08-03 11:27:50 -0400

    Finish camera implementation (see commit message)
    - We can now take a picture and bring it to the next activity
    - It runs like shit but that might just be because it's running inside the emulator

--- app/src/main/java/io/github/incplusplus/potwhole/CameraActivity.java
+++ app/src/main/java/io/github/incplusplus/potwhole/CameraActivity.java
@@ -1,13 +1,20 @@
 package io.github.incplusplus.potwhole;
 
+import static io.github.incplusplus.potwhole.MainActivity.TAG;
+
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.util.Size;
 import android.widget.Button;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +30,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 import com.google.common.util.concurrent.ListenableFuture;
+import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -110,27 +121,60 @@ public class CameraActivity extends AppCompatActivity {
         cameraProvider.bindToLifecycle(
                 (LifecycleOwner) this, cameraSelector, imageAnalysis, preview, imageCapture);
 
+        String FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS";
+        String name =
+                new SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis());
+
+        File outputDir = CameraActivity.this.getCacheDir(); // context being the Activity pointer
+        File outputFile = null;
+        try {
+            outputFile = File.createTempFile(name, ".jpg", outputDir);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to create temporary file for picture to be stored in.", e);
+            Toast.makeText(
+                            CameraActivity.this,
+                            "An error occurred. Do we have storage permissions?",
+                            Toast.LENGTH_SHORT)
+                    .show();
+            // Kick the user back to the map fragment
+            Intent intent = new Intent(CameraActivity.this, MapFragment.class);
+            startActivity(intent);
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name);
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg");
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+            contentValues.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image");
+        }
+
+        ImageCapture.OutputFileOptions outputOptions =
+                new ImageCapture.OutputFileOptions.Builder(outputFile).build();
+
         captureButton.setOnClickListener(
                 v -> {
                     imageCapture.takePicture(
-                            executor,
-                            new ImageCapture.OnImageCapturedCallback() {
+                            outputOptions,
+                            ContextCompat.getMainExecutor(this),
+                            new ImageCapture.OnImageSavedCallback() {
                                 @Override
-                                public void onCaptureSuccess(@NonNull ImageProxy image) {
-                                    super.onCaptureSuccess(image);
-                                    // If this doesn't work it's something to do with needing to set
-                                    // rotation on the images taken
-                                    Bitmap bitmap = toBitmap(image);
+                                public void onImageSaved(
+                                        @NonNull ImageCapture.OutputFileResults outputFileResults) {
+                                    String msg = "Photo capture succeeded: ${output.savedUri}";
+                                    //
+                                    // Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                                    Log.d(TAG, msg);
+
                                     Intent intent =
                                             new Intent(
                                                     CameraActivity.this, ImageConfirmation.class);
-                                    intent.putExtra("BitmapImage", bitmap);
+                                    intent.putExtra("photoUri", outputFileResults.getSavedUri());
                                     startActivity(intent);
                                 }
 
                                 @Override
-                                public void onError(@NonNull ImageCaptureException error) {
-                                    error.printStackTrace();
+                                public void onError(@NonNull ImageCaptureException exception) {
+                                    Log.e(TAG, "Photo capture failed: ${exc.message}", exception);
                                 }
                             });
                 });
@@ -163,10 +207,4 @@ public class CameraActivity extends AppCompatActivity {
         ActivityCompat.requestPermissions(this, CAMERA_PERMISSION, CAMERA_REQUEST_CODE);
         ActivityCompat.requestPermissions(this, STORAGE_PERMISSION, STORAGE_REQUEST_CODE);
     }
-
-    // start camera activity in CameraActivity
-    //    private void enableCamera() {
-    //        Intent intent = new Intent(this, CameraActivity.class);
-    //        startActivity(intent);
-    //    }
 }
